d88d03b5cc762a1dd4f64ddab5d16738be8f4355
Veronica <vevef.santos@hotmail.com>
2025-03-04 18:07:11 -0300

    Initial commit

---
+++ Checklist/src/test/java/me/dio/Checklist/ChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Checklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
